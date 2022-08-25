467176acb2cdcbad6fd2f67d27f0126bb86709c0
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-08-25 08:15:16 +0300

    try work with modals

--- packages/component/src/components/PEModal.vue
+++ packages/component/src/components/PEModal.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="modal show" style="display: block">
-    <slot />
-    <button type="button" @click="$modal.show('PEModal')">add</button>
-    <button type="button" @click="$modal.hide(modal.id)">hide</button>
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" aria-label="Close" @click="$modal.hide(modal.id)">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p><slot /></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="$modal.show('PEModal')">add</button>
+          <button type="button" class="btn btn-secondary" @click="$modal.hide(modal.id)">hide</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
